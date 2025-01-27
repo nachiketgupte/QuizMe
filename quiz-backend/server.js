0b3ef99fff346d36812e81e54d0ebabebe25f7a5
unknown <nachiketgupte8@gmail.com>
2025-01-27 21:28:07 +0530

    Added docker

--- quiz-backend/server.js
+++ quiz-backend/server.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = 5000;
 
 app.get("/", (req, res) => {
-    return res.json({message: "Hey! I am in nodejs container."});
+    return res.json({message: "Hey! I am in nodejs container v2."});
 })
 
 app.listen(PORT, () => console.log(`Server running on PORT: ${PORT}`));
